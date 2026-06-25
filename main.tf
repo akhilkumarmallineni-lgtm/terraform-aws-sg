4b3efdbea78d2ee6fd10fac2bdefb3052e18c945
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-06-25 22:26:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "main" {
-  name        = var.sg_name
-  vpc_id      = var.vpc_id
+  name        = local.common_name
   description = "Allow traffic for ${var.sg_name} project ${var.project} in ${var.environment}"
+  vpc_id      = var.vpc_id
 
   egress {
     from_port        = 0
@@ -9,7 +9,8 @@ resource "aws_security_group" "main" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-  tags = merge(
+
+  tags = merge (
     var.sg_tags,
     local.common_tags
   )
